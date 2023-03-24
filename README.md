1d86c3867ccfbcb9cf0f66d68da1ffe993a86899
Sachin Jadhav <sachinjadhav@Sachins-MacBook-Pro.local>
2023-03-24 21:25:37 +0530

    Update the return statement and readme file

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ This project contains two challenges - a "Ticket Breakdown" challenge and a "Ref
 
 ## [Refactoring](Refactoring.md)
 
+- No need to set 'candidate = TRIVIAL_PARTITION_KEY' manually inside else part, we can just move it to return statement and return it conditionally at the end.
 - Changed the variable TRIVIAL_PARTITION_KEY to meaningful name.
 - Function input variable name changed to partition, because we are passing the useful partition(not any random event) and then doing the further operation on the key.
 - Candidate variable name was a confusing one and so I have replaced it with "partitionKeyOutput". W=
--- dpk.js
+++ dpk.js
@@ -17,19 +17,16 @@ exports.deterministicPartitionKey = (partition) => {
     }
   }
 
-  if (partitionKeyOutput) {
-    if (typeof partitionKeyOutput !== "string") {
-      partitionKeyOutput = JSON.stringify(partitionKeyOutput);
-    }
-  } else {
-    partitionKeyOutput = DEFAULT_PARTITION_KEY;
+  if (partitionKeyOutput && typeof partitionKeyOutput !== "string") {
+    partitionKeyOutput = JSON.stringify(partitionKeyOutput);
   }
 
-  if (partitionKeyOutput.length > MAX_PARTITION_KEY_LENGTH) {
+  if (partitionKeyOutput?.length > MAX_PARTITION_KEY_LENGTH) {
     partitionKeyOutput = crypto
       .createHash("sha3-512")
       .update(partitionKeyOutput)
       .digest("hex");
   }
-  return partitionKeyOutput;
+
+  return partitionKeyOutput || DEFAULT_PARTITION_KEY;
 };
